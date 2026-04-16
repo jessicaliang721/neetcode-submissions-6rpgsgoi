54ffa48ce2fdc6de7732270053bd92cadc174bbd
Jessica Liang <10553891+jessicaliang721@users.noreply.github.com>
2026-04-16 14:29:52 -0500

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        // log frequency of each letter
+        const freqMap = {};
+        let L = 0;
+        let maxLength = 0;
+        let maxFreq = 0;
+
+        for (let R = 0; R < s.length; R++) {
+            // add s[R] to frequency tracker
+            if (freqMap[s[R]] === undefined) {
+                freqMap[s[R]] = 1;
+            } else {
+                freqMap[s[R]]+=1;
+            }
+
+            maxFreq = Math.max(maxFreq, freqMap[s[R]])
+
+            // use window length
+            // invalid condition
+            while ((R - L + 1) - maxFreq > k) {
+                // decrement s[L]'s count in frequency tracker
+                freqMap[s[L]]-=1;
+                // we need to shrink the window
+                L++;
+            }
+
+            // update maxLen
+            maxLength = Math.max(maxLength, R-L+1)
+        }
+
+        return maxLength;
+    }
+}
