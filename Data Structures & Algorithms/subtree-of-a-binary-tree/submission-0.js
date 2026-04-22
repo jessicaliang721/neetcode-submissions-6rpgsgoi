1e90c519d30e5009ff560127d628f8689cdab780
Jessica Liang <10553891+jessicaliang721@users.noreply.github.com>
2026-04-22 09:31:11 -0500

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} subRoot
+     * @return {boolean}
+     */
+    isSubtree(root, subRoot) {
+        if (!root) return false; // immediately return false if root is null because subtree will never be in it
+
+        // is the current node the start of the subroot?
+        // base case is where the current node is the start of the subroot
+        if (this.sameTree(root, subRoot)) return true;
+        
+        // if no, traverse left child and ask if the current node is the start of the subroot
+        // else traverse right child and ask if the current node is the start of the subroot
+        return this.isSubtree(root.left, subRoot) || this.isSubtree(root.right, subRoot);
+    }
+
+    sameTree(root1, root2) { // this is assuming that we found the same root node
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        if (root1.val !== root2.val) {
+            return false;
+        }
+
+        const leftMatches = this.sameTree(root1.left, root2.left);
+        const rightMatches = this.sameTree(root1.right, root2.right);
+
+        return leftMatches && rightMatches;
+    }
+}
